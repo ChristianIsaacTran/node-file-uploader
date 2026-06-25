66822b4291361f6aacc214f14410210809b179ef
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-06-24 23:20:01 -0500

    FEAT: added "current folder and current filepath" to folder.ejs

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -9,20 +9,25 @@ async function createRootFolder(req, res) {
 
 // get the contents of the current folder route
 async function dynamicFolderDisplay(req, res) {
-  let currentRoute = req.params.filepath; 
+  let currentRoute = req.params.filepath;
 
   const previousRoute = `${currentRoute.slice(0, -2).join("/")}/`;
-  
+
   currentRoute = currentRoute.join("/");
   const currentFolder = await db.getFolder(currentRoute, req.user.id);
 
   const files = await db.getFiles(currentFolder.id);
 
-  const subFolders = await db.getSubFolders(currentRoute, req.user.id, currentFolder.folderName);
+  const subFolders = await db.getSubFolders(
+    currentRoute,
+    req.user.id,
+    currentFolder.folderName,
+  );
 
   res.render("folder", {
     files: files,
     currentRoute: currentRoute,
+    currentFolderName: currentFolder.folderName,
     subFolders: subFolders,
     previousRoute: previousRoute,
   });
